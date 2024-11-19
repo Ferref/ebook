ac1fde19537736f895ca8b2d7fa218759bd878b5
Ferref <kovacsferenc026@gmail.com>
2024-11-19 10:58:47 +0100

    screens mappa, (home, makrket, profile) hozzaadva

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "DuckReader",
+          style: Theme.of(context).textTheme.labelLarge,
+        ),
+      ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(14),
+          child: Column(
+            children: [
+              Text("Home screen"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
